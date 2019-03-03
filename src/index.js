b189c785edc64a458a753fa5a7a13be4240b9a28
Avaer Kazmer <a@modules.io>
2019-03-03 17:55:04 -0500

    Re-add missing xrState id

--- src/index.js
+++ src/index.js
@@ -241,6 +241,7 @@ const xrState = (() => {
     }
     return result;
   })();
+  result.id = _makeTypedArray(Uint32Array, 1);
   result.fakeVrDisplayEnabled = _makeTypedArray(Uint32Array, 1);
   
   return result;
